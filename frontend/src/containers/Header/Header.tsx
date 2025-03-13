cfd058c1137e67bcca38cd195015dab29b671c61
nelli gorlova <nelli7035@gmail.com>
2025-03-14 00:32:31 +0600

    Added Container to the correct location

--- frontend/src/containers/Header/Header.tsx
+++ frontend/src/containers/Header/Header.tsx
@@ -1,10 +1,13 @@
 import CategoryNavbar from '../../components/Navbar/CategoryNavbar.tsx';
 import MainToolbar from '../../components/Toolbar/Toolbar.tsx';
+import { Container } from '@mui/material';
 
 const Header = () => {
   return (
     <div>
-      <MainToolbar/>
+      <Container>
+        <MainToolbar/>
+      </Container>
       <CategoryNavbar/>
     </div>
   );
