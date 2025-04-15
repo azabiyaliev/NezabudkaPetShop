24cacf231492ef95d8a7177e31b22c672f28ff20
nelli gorlova <nelli7035@gmail.com>
2025-04-16 04:06:16 +0600

    Added a third level of nesting

--- frontend/src/features/Category/CategoryMenuBox/CategoryMenuBox.tsx
+++ frontend/src/features/Category/CategoryMenuBox/CategoryMenuBox.tsx
@@ -5,6 +5,7 @@ import "./CategoryMenuBox.css";
 import paw from "../../.../../../assets/paw-solid-svgrepo-com.svg";
 import { fetchCategoriesThunk } from '../../../store/categories/categoriesThunk.ts';
 import { Subcategory } from '../../../types';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 
 const CategoryMenuBox = () => {
@@ -12,6 +13,7 @@ const CategoryMenuBox = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("Собаки");
   const dispatch = useAppDispatch();
   const [hoveredSubcategory, setHoveredSubcategory] = useState<Subcategory | null>(null);
+  const [hoveredNestedSubcategory, setHoveredNestedSubcategory] = useState<Subcategory | null>(null);
 
   const handleCategoryClick = (categoryTitle: string) => {
     setSelectedCategory(categoryTitle);
@@ -24,8 +26,6 @@ const CategoryMenuBox = () => {
   const currentCategory = categories.find(
     (cat) => cat.title === selectedCategory,
   );
-
-  console.log(categories);
 
   return (
     <div className="menu-box mt-5 col-lg-3 col-md-4 d-none d-md-block">
@@ -65,11 +65,14 @@ const CategoryMenuBox = () => {
             key={sub.id}
             className="subcategory-item-in-box"
             onMouseEnter={() => setHoveredSubcategory(sub)}
-            onMouseLeave={() => setHoveredSubcategory(null)}
-            style={{position: "relative"}}
+            onMouseLeave={() => {
+              setHoveredSubcategory(null);
+              setHoveredNestedSubcategory(null);
+            }}
+            style={{ position: "relative" }}
           >
             <a href="#" className="nav-category-link">
-              <img src={paw} alt="paw icon" className="paw-icon"/>
+              <img src={paw} alt="paw icon" className="paw-icon" />
               {sub.title}
             </a>
 
@@ -78,16 +81,46 @@ const CategoryMenuBox = () => {
               sub.subcategories.length > 0 && (
                 <div className="nested-subcategories">
                   {sub.subcategories.map((nested) => (
-                    <a key={nested.id} href="#" className="nav-category-link">
-                      {nested.title}
-                    </a>
+                    <div
+                      key={nested.id}
+                      className="nested-subcategory-item"
+                      onMouseEnter={() => setHoveredNestedSubcategory(nested)}
+                      onMouseLeave={() => setHoveredNestedSubcategory(null)}
+                      style={{ position: "relative" }}
+                    >
+                      <a
+                        href="#"
+                        className="nav-category-link d-flex justify-content-between align-items-center"
+                      >
+                        <span>{nested.title}</span>
+                        {nested.subcategories &&
+                          nested.subcategories.length > 0 && (
+                            <KeyboardArrowRightIcon fontSize="small" />
+                          )}
+                      </a>
+
+                      {hoveredNestedSubcategory?.id === nested.id &&
+                        nested.subcategories &&
+                        nested.subcategories.length > 0 && (
+                          <div className="third-level-subcategories">
+                            {nested.subcategories.map((third) => (
+                              <a
+                                key={third.id}
+                                href="#"
+                                className="nav-category-link"
+                              >
+                                {third.title}
+                              </a>
+                            ))}
+                          </div>
+                        )}
+                    </div>
                   ))}
                 </div>
               )}
           </div>
         ))}
       </div>
-
     </div>
   );
 };
