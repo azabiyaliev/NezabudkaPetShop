49914ef31634ee3a6bb5b76e9282c10916f85640
kiryuusuke <marsel14082006@gmail.com>
2025-04-22 23:15:06 +0600

    added global styles

---
+++ frontend/src/globalStyles/globalTheme.ts
@@ -0,0 +1,10 @@
+import { COLORS, FONTS, MEDIA_REQ, SPACING } from './stylesObjects.ts';
+
+const theme = {
+  colors: COLORS,
+  spacing: SPACING,
+  fonts: FONTS,
+  media: MEDIA_REQ,
+};
+
+export default theme;
---
+++ frontend/src/globalStyles/stylesObjects.ts
@@ -0,0 +1,52 @@
+export const COLORS = {
+  primary: '#216A12',
+  contrastText: '#FFFFFF',
+  secondary: '#408E2F',
+  success: '#28a745',
+  yellow: '#FFC000',
+  error: '#840003',
+  warning: '#C50005',
+  info: '#17a2b8',
+  text: '#212529',
+  background: '#f8f9fa',
+  white: '#ffffff',
+  black: '#000000',
+}
+
+export const SPACING = {
+  xs: '10px',
+  sm: '20px',
+  md: '25px',
+  lg: '35px',
+  xl: '40px',
+  xxl: '50px',
+}
+
+export const FONTS = {
+  size: {
+    xs: '12px',
+    sm: '14px',
+    default: '16px',
+    lg: '20px',
+    xl: '24px',
+    xxl: '30px',
+  },
+  weight: {
+    normal: 400,
+    medium: 600,
+    bold: 700,
+  }
+}
+
+export const MEDIA_REQ = {
+  mobile: {
+    sm: '320px',
+    md: '360px',
+    lg: '414px',
+  },
+  tablet: {
+    sm: '600px',
+    md: '768px',
+    lg: '900px',
+  },
+}
