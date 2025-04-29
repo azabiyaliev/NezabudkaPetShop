2c1c309b1cbf08fb083b73c0e9751de65c41a999
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2025-04-29 14:22:31 +0600

    [FRONT] After logging into the application as a super admin, I removed the "Info by admin" and displayed the order statistics instead.

--- frontend/src/features/Admin/AdminProfile/AdminProfile.tsx
+++ frontend/src/features/Admin/AdminProfile/AdminProfile.tsx
@@ -1,22 +1,34 @@
 import AdminBar from "./AdminBar.tsx";
+import { useAppSelector } from '../../../app/hooks.ts';
+import { selectUser } from '../../../store/users/usersSlice.ts';
+import { userRoleAdmin } from '../../../globalConstants.ts';
+import OrderStats from '../AdminOrderPage/OrderStats.tsx';
 
 const AdminProfile = () => {
+  const user = useAppSelector(selectUser);
+
   return (
-    <div>
-      <div className="d-flex ">
-        <div className="col-3 ">
-          <AdminBar />
-        </div>
-        <div className="col-9">
-          <h2 className="text-uppercase text-center mt-4">Личный кабинет</h2>
-          <div>
-            <h6 style={{textAlign: "center", marginTop: "20px"}}>
-              Info by admin
-            </h6>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      {user && (
+        <>
+          {user.role === userRoleAdmin ?
+            <div className="d-flex ">
+              <div className="col-3 ">
+                <AdminBar/>
+              </div>
+              <div className="col-9">
+                <h2 className="text-uppercase text-center mt-4">Личный кабинет</h2>
+                <div>
+                  <h6 style={{textAlign: 'center', marginTop: '20px'}}>
+                    Info by super admin
+                  </h6>
+                </div>
+              </div>
+            </div> : <OrderStats/>
+          }
+        </>
+      )}
+    </>
   );
 };
 
