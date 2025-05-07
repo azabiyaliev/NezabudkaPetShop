03a6f01acb3546dd9ccbab30230200eba361a77c
nelli gorlova <nelli7035@gmail.com>
2025-05-07 13:52:16 +0600

    Fixed main container

--- frontend/src/components/Layout/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import Header from '../Header/Header.tsx';
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
 import Footer from '../Footer/Footer.tsx';
 
 interface LayoutProps extends PropsWithChildren {
@@ -38,7 +38,7 @@ const Layout: React.FC<LayoutProps> = ({ children, beforeContainer, isWide }) =>
             {children}
           </Box>
         ) : (
-          <Container maxWidth="xl">{children}</Container>
+          <Box>{children}</Box>
         )}
       </main>
       <footer>
